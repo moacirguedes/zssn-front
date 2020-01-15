676bcc0c0faef8fc720e60b1b98ad1bb7cbc7768
Moacir <guedesmoacir12@gmail.com>
2020-01-15 10:58:54 -0300

    Survivors loading

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
 import './styles.css';
+import { getSurvivors } from '../../model/survivor';
 
 export default class Main extends Component {
+  state = {
+    survivors: []
+  }
+
+  componentDidMount() {
+    this.loadSurvivors();
+  }
+
+  loadSurvivors = async () => {
+    const data = await getSurvivors();
+
+    this.setState({
+      survivors: data
+    });
+  }
+
   render() {
-    return (<h1>Hey</h1>);
+    return (
+      <div>
+        {this.state.survivors.map(survivor => 
+          <li key={survivor.location}>
+            {survivor.name}
+          </li>
+        )}
+      </div>
+    );
   }
 }
