f6d0e84313b09c188aa52f74c397fae0ee9c5551
Moacir <guedesmoacir12@gmail.com>
2020-01-16 10:40:55 -0300

    Add router to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Main from './pages/main';
 import Header from './components/header';
+import Routes from './routes';
 
 function App() {
   return (
     <div>
       <Header />
-      <Main />
+      <Routes />
     </div>
   );
 }
