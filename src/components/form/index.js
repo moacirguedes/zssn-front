e9b6621bacdc7c2e7fd4059dc0903569538ea975
Moacir <guedesmoacir12@gmail.com>
2020-01-22 11:03:31 -0300

    refactoring create survivor css

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './style.css';
 
-const Form = (props) => 
+const Form = (props) =>
   <form
     className="NewSurvivorForm"
     onSubmit={props.handleSubmit}
   >
+    <label className="CreateLabel">Name</label>
     <input
-      placeholder="Name"
       required
       name="name"
       value={props.name}
@@ -15,81 +15,108 @@ const Form = (props) =>
     />
 
     <div className="LineInputs">
-      <input
-        className="Input__Small"
-        placeholder="Age"
-        type="number"
-        min="2"
-        max="100"
-        required
-        value={props.age}
-        onChange={props.handleChange}
-        name="age"
-      />
+      <div class="LabelInputDiv">
+        <label className="CreateLabel">Age</label>
+        <br />
 
-      <select
-        className="Input__Small"
-        value={props.gender}
-        onChange={props.handleChange}
-        name="gender"
-      >
-        <option value="F">Female</option>
-        <option value="M">Male</option>
-      </select>
+        <input
+          className="CreateSurvivorInput"
+          type="number"
+          min="2"
+          max="100"
+          required
+          value={props.age}
+          onChange={props.handleChange}
+          name="age"
+        />
+      </div>
 
-      <input
-        placeholder="Location"
-        value={props.lonlat}
-        onChange={props.handleChange}
-        name="lonlat"
-        className="Input__Medium"
-      />
+      <div class="LabelInputDiv">
+        <label className="CreateLabel">Gender</label>
+        <br />
+
+        <select
+          className="CreateSurvivorInput"
+          value={props.gender}
+          onChange={props.handleChange}
+          name="gender"
+        >
+          <option value="F">Female</option>
+          <option value="M">Male</option>
+        </select>
+      </div>
+
+      <div class="LabelInputDiv">
+        <label className="CreateLabel">Location</label>
+        <br />
+
+        <input
+          value={props.lonlat}
+          onChange={props.handleChange}
+          name="lonlat"
+          className="CreateSurvivorInput"
+        />
+      </div>
     </div>
 
-    <div className="LineInputs">
-      <input
-        className="Input__Small"
-        placeholder="Water"
-        type="number"
-        min="0"
-        required
-        value={props.water}
-        onChange={props.handleChange}
-        name="water"
-      />
+    <div className="LineInputs height140">
+      <div className="ItensDiv">
+        <label className="CreateLabel">Water</label>
+        <br />
 
-      <input
-        className="Input__Small"
-        placeholder="Food"
-        type="number"
-        min="0"
-        required
-        value={props.food}
-        onChange={props.handleChange}
-        name="food"
-      />
+        <input
+          className="CreateItemInput"
+          type="number"
+          min="0"
+          required
+          value={props.water}
+          onChange={props.handleChange}
+          name="water"
+        />
 
-      <input
-        className="Input__Small"
-        placeholder="Medication"
-        type="number"
-        min="0"
-        required
-        value={props.medication}
-        onChange={props.handleChange}
-        name="medication"
-      />
+        <br />
+        <label className="CreateLabel">Food</label>
+        <br />
 
-      <input
-        className="Input__Small"
-        placeholder="Ammunition"
-        type="number"
-        min="0"
-        required
-        value={props.ammunition}
-        onChange={props.handleChange}
-        name="ammunition"
-      />
+        <input
+          className="CreateItemInput"
+          type="number"
+          min="0"
+          required
+          value={props.food}
+          onChange={props.handleChange}
+          name="food"
+        />
+      </div>
+
+      <div className="ItensDiv">
+        <label className="CreateLabel">Medication</label>
+        <br />
+
+        <input
+          className="CreateItemInput"
+          type="number"
+          min="0"
+          required
+          value={props.medication}
+          onChange={props.handleChange}
+          name="medication"
+        />
+
+        <br />
+        <label className="CreateLabel">Ammunition</label>
+        <br />
+
+        <input
+          className="CreateItemInput"
+          type="number"
+          min="0"
+          required
+          value={props.ammunition}
+          onChange={props.handleChange}
+          name="ammunition"
+        />
+      </div>
     </div>
 
     <button>Create</button>
