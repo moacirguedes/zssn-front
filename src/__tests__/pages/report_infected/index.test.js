1ad36bb504351b25b6783ed9f81353747a414e84
Moacir <guedesmoacir12@gmail.com>
2020-01-28 15:20:24 -0300

    Report test

--- src/__tests__/pages/report_infected/index.test.js
+++ src/__tests__/pages/report_infected/index.test.js
@@ -1,11 +1,50 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import ReportInfected from '../../../pages/report_infected';
+import faker from 'faker';
+import * as api from '../../../model/survivor';
 
 describe('<ReportInfected />', () => {
   it('should render correctly', () => {
     const wrapper = shallow(<ReportInfected />);
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('when select two survivors', () => {
+    it('send report', () => {
+      const survivors = [
+        {
+          infected: false,
+          location: faker.random.uuid(),
+          name: faker.name.firstName()
+        },
+        {
+          infected: false,
+          location: faker.random.uuid(),
+          name: faker.name.firstName()
+        }
+      ]
+
+      jest.spyOn(api, 'reportInfected');
+
+      const wrapper = mount(<ReportInfected />);
+
+      wrapper.setState({
+        survivors: survivors
+      });
+
+      wrapper.find('select').at(0).simulate('change', {
+        target: { value: survivors[0].location, name: 'reporter' }
+      });
+
+      wrapper.find('select').at(1).simulate('change', {
+        target: { value: survivors[1].location, name: 'infected' }
+      });
+
+      wrapper.find('form').simulate('submit');
+
+      expect(api.reportInfected).toHaveBeenCalledWith(survivors[0].location, survivors[1].location);
+    });
   });
 });
